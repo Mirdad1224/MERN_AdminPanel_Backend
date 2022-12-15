9ac9c6c95f38a2c6beef02a3e3f1d52e042efd24
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-15 15:30:53 +0330

    setup mongodb connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -5,8 +6,13 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const PORT = process.env.PORT || 8000
+
+const connectDB = require('./config/dbConnection');
+
+connectDB()
+
 //* configurations
-dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
@@ -27,4 +33,11 @@ app.use('/management', managementRoutes)
 app.use('/sales', salesRoutes)
 
 
-app.listen()
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+})
