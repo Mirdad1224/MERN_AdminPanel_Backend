a3faeb3c2b875b145cdde6d4dfb830bf72fb8253
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-17 14:12:51 +0330

    refine general routes

--- routes/general.js
+++ routes/general.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const router = express.Router()
 
+const { getUser, getDashboardStats } = require("../controllers/general");
+
+router.get("/user/:id", getUser);
+router.get("/dashboard", getDashboardStats);
+
+
 module.exports = router
