9da209cc73f6a01ed3d22241610e95f331574072
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-17 14:13:04 +0330

    refine management routes

--- routes/management.js
+++ routes/management.js
@@ -1,5 +1,10 @@
-const express = require('express');
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-module.exports = router
+const { getAdmins, getUserPerformance } = require("../controllers/management");
+
+router.get("/admins", getAdmins);
+router.get("/performance/:id", getUserPerformance);
+
+module.exports = router;
